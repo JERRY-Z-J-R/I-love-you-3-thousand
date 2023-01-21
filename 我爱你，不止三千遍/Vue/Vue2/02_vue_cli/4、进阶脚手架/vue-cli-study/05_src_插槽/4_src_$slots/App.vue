61e3a1a195077212156262ce65e08a08fca5008a
JERRY-Z-J-R <www.zjr1846334075@foxmail.com>
2023-01-21 14:01:26 +0800

    udpate

---
+++ 我爱你，不止三千遍/Vue/Vue2/02_vue_cli/4、进阶脚手架/vue-cli-study/05_src_插槽/4_src_$slots/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="app">
+        <h1>我是APP组件</h1>
+        <Category>
+            <span>1</span>
+            <span>2</span>
+        </Category>
+        <Category>
+            <template v-slot:test1>
+                <span>1</span>
+                <span>1</span>
+            </template>
+            <template v-slot:test2>
+                <span>2</span>
+                <span>2</span>
+            </template>
+        </Category>
+    </div>
+</template>
+
+<script>
+import Category from './components/Category';
+
+export default {
+    name: 'App',
+    components: { Category }
+};
+</script>
+
+<style scoped>
+.app {
+    height: 400px;
+    background-color: rgb(168, 168, 168);
+}
+</style>
---
+++ 我爱你，不止三千遍/Vue/Vue2/02_vue_cli/4、进阶脚手架/vue-cli-study/05_src_插槽/4_src_$slots/components/Category.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="category">
+        <h1>我是Category组件</h1>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Category',
+    mounted() {
+        console.log(this);
+    }
+};
+</script>
+
+<style scoped>
+.category {
+    height: 100px;
+    background-color: #993366;
+}
+</style>
