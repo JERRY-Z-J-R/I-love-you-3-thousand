b8bde8c4a8730a472cc7825288d45e329d541681
JERRY-Z-J-R <www.zjr1846334075@foxmail.com>
2023-01-21 12:25:35 +0800

    update vue2

---
+++ 我爱你，不止三千遍/Vue/Vue2/02_vue_cli/2、分析脚手架/hello-world/03_src_props配置项/components/Teacher.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>
+            {{ $attrs.name }}
+        </h1>
+        <h1>
+            {{ age }}
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Teacher',
+    data() {
+        return {
+            age: this.$attrs.age
+        };
+    },
+    methods: {
+        updateAge() {
+            this.age++;
+        }
+    }
+};
+</script>
+
+<style scoped></style>
