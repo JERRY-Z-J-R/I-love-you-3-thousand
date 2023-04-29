90e81635c2efbd57a9892592d456fcb660e37dcf
JERRY-Z-J-R <www.zjr1846334075@foxmail.com>
2023-04-29 18:05:43 +0800

    update

--- 我爱你，不止三千遍/Vue/Vue2/02_vue_cli/2、分析脚手架/hello-world/03_src_props配置项/components/Teacher.vue
+++ 我爱你，不止三千遍/Vue/Vue2/02_vue_cli/2、分析脚手架/hello-world/03_src_props配置项/components/Teacher.vue
@@ -6,6 +6,7 @@
         <h1>
             {{ age }}
         </h1>
+        <button @click="updateAge">尝试修改收到的年龄</button>
     </div>
 </template>
 
@@ -14,11 +15,17 @@ export default {
     name: 'Teacher',
     data() {
         return {
+            // 通过 data 数据来接收
+            // $attrs 内的属性不会出现在 this 上，所以可以同名（age）
             age: this.$attrs.age
         };
     },
     methods: {
         updateAge() {
+            // 直接修改 $attrs 的属性是不会有响应式的！
+            // this.$attrs.age++;
+            // 这样也没用！
+            // this.$set(this.$attrs, 'age', this.$attrs.age + 1);
             this.age++;
         }
     }
